d86f8700d86919edf6ff30dc6b82bae871f75b17
Greg Look <greg@greg-look.net>
2015-11-05 00:26:52 -0800

    Check that hash-fn returns a multihash in read!.

--- src/blocks/core.clj
+++ src/blocks/core.clj
@@ -92,10 +92,14 @@
    (read! source :sha2-256))
   ([source algorithm]
    (let [hash-fn (resolve-hash! algorithm)
-         content (bytes/to-byte-array source)]
+         content (bytes/to-byte-array source)
+         id (hash-fn content)]
      (when-not (empty? content)
-       (Block. (hash-fn content)
-               (PersistentBytes/wrap content))))))
+       (when-not (instance? Multihash id)
+         (throw (RuntimeException.
+                  (str "Block identifier must be a Multihash, "
+                       "hashing algorithm returned: " (pr-str id)))))
+       (Block. id (PersistentBytes/wrap content))))))
 
 
 (defn write!
