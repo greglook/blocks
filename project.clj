428e689741b45618e120abcf46dc0f6d94f550fe
Greg Look <greg@greg-look.net>
2016-04-27 21:54:53 -0700

    Set release version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/blocks "0.7.0-SNAPSHOT"
+(defproject mvxcvi/blocks "0.7.0"
   :description "Content-addressed data storage interface."
   :url "https://github.com/greglook/blocks"
   :license {:name "Public Domain"
