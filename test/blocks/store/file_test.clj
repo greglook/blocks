8d7061b8af3e5736326baf904838fe9db244f031
Greg Look <greg@greg-look.net>
2017-03-12 21:33:56 -0700

    Add test exercising file store prefix listing.

--- test/blocks/store/file_test.clj
+++ test/blocks/store/file_test.clj
@@ -1,10 +1,17 @@
 (ns blocks.store.file-test
   (:require
+    [blocks.core :as block]
     (blocks.store
       [file :as file :refer [file-block-store]]
       [tests :as tests])
     [clojure.java.io :as io]
     [clojure.test :refer :all]))
+
+
+(defn- mk-tmpdir!
+  []
+  (io/file "target" "test" "tmp"
+           (str "file-block-store." (System/currentTimeMillis))))
 
 
 (deftest conversion-tests
@@ -18,10 +25,23 @@
           "should return nil for non-hex files"))))
 
 
+(deftest file-listing
+  (let [store (file-block-store (mk-tmpdir!))
+        a (block/read! "larry")
+        b (block/read! "curly")
+        c (block/read! "moe")]
+    (block/put! store a)
+    (block/put! store b)
+    (block/put! store c)
+    (is (= 3 (count (block/list store :after "12200c"))))
+    (is (= 3 (count (block/list store :after "12200d0980"))))
+    (is (= 1 (count (block/list store :after "12204b6f51"))))
+    (is (= (:id b) (:id (first (block/list store :after "12204b6f51")))))
+    (is (empty? (block/list store :after "122064")))))
+
+
 (deftest ^:integration test-file-store
-  (let [tmpdir (io/file "target" "test" "tmp"
-                        (str "file-block-store."
-                          (System/currentTimeMillis)))]
+  (let [tmpdir (mk-tmpdir!)]
     (tests/check-store!
       #(file-block-store tmpdir)
       :eraser file/erase!)))
