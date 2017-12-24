8cd245e37478d9e00d2e2ca775cd8a0cfe530409
Greg Look <greg@greg-look.net>
2017-12-24 12:18:35 -0800

    Bump SNAPSHOT version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/blocks "1.1.0-SNAPSHOT"
+(defproject mvxcvi/blocks "1.1.1-SNAPSHOT"
   :description "Content-addressed data storage interface."
   :url "https://github.com/greglook/blocks"
   :license {:name "Public Domain"
