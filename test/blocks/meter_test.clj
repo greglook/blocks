675e9a4924059094fb8d63373d30a1cb2bf5b5e9
Greg Look <greg@greg-look.net>
2018-07-25 00:06:35 -0700

    Basic store metering tests.

--- test/blocks/meter_test.clj
+++ test/blocks/meter_test.clj
@@ -7,20 +7,18 @@
     [clojure.test :refer :all]))
 
 
-#_
 (deftest store-behavior
-  (let [underlying (memory-block-store)
-        store (metered-block-store
-                (fn record!
-                  [store event]
-                  (swap! (:events store) conj event))
-                :store underlying
-                :events (atom []))
+  (let [store (assoc (memory-block-store)
+                     ::events (atom [])
+                     ::meter/recorder
+                     (fn record!
+                       [store event]
+                       (swap! (::events store) conj event)))
         a (block/read! "foo bar baz")
         b (block/read! "abracadabra")
         c (block/read! "123 xyz")]
     (block/put! store a)
     (block/put! store b)
-    (prn @(:events store))
     ; TODO: check events
+    ;(prn @(::events store))
     ))
