ef673b877fa23deeb9a4bebe4686dabd29d3a99c
Greg Look <greg@amperity.com>
2019-02-02 15:47:06 -0800

    Update replica store tests.

--- src/blocks/store/replica.clj
+++ src/blocks/store/replica.clj
@@ -8,14 +8,22 @@
     [manifold.deferred :as d]))
 
 
+(defn- resolve-stores
+  "Resolve the configured replica stores."
+  ([store]
+   (resolve-stores store (:replicas store)))
+  ([store replicas]
+   (mapv (partial get store) replicas)))
+
+
 (defrecord ReplicaBlockStore
-  [store-keys]
+  [replicas]
 
   component/Lifecycle
 
   (start
     [this]
-    (when-let [missing (seq (remove (partial contains? this) store-keys))]
+    (when-let [missing (seq (remove (partial contains? this) replicas))]
       (throw (IllegalStateException.
                (str "Replica block store is missing configured keys: "
                     (pr-str missing)))))
@@ -31,38 +39,38 @@
 
   (-list
     [this opts]
-    (->> (map this store-keys)
+    (->> (resolve-stores this)
          (map #(block/list % opts))
          (apply store/merge-blocks)))
 
 
   (-stat
     [this id]
-    (store/some-store (mapv this store-keys) block/stat id))
+    (store/some-store (resolve-stores this) block/stat id))
 
 
   (-get
     [this id]
     ; OPTIMIZE: query in parallel, use `d/alt`?
-    (store/some-store (mapv this store-keys) block/get id))
+    (store/some-store (resolve-stores this) block/get id))
 
 
   (-put!
     [this block]
     (d/chain
-      (block/put! (get this (first store-keys)) block)
+      (block/put! (get this (first replicas)) block)
       (fn keep-preferred
         [stored]
         (let [block (store/preferred-block block stored)]
           (d/chain
-            (store/zip-stores (mapv this (rest store-keys)) block/put! block)
-            (constantly stored))))))
+            (store/zip-stores (resolve-stores this (rest replicas)) block/put! block)
+            (partial apply store/preferred-block stored))))))
 
 
   (-delete!
     [this id]
     (d/chain
-      (store/zip-stores (mapv this store-keys) block/delete! id)
+      (store/zip-stores (resolve-stores this) block/delete! id)
       (partial some true?)
       boolean)))
 
@@ -76,7 +84,7 @@
 (defn replica-block-store
   "Creates a new replica block store which will persist blocks to multiple
   backing stores. Block operations will be performed on the stores in the order
-  given in `store-keys`, where each key is looked up in the store record."
-  [store-keys & {:as opts}]
+  given in `replicas`, where each key is looked up in the store record."
+  [replicas & {:as opts}]
   (map->ReplicaBlockStore
-    (assoc opts :store-keys (vec store-keys))))
+    (assoc opts :replicas (vec replicas))))
--- test/blocks/store/replica_test.clj
+++ test/blocks/store/replica_test.clj
@@ -8,13 +8,13 @@
     [com.stuartsierra.component :as component]))
 
 
-#_
-(deftest store-construction
+(deftest lifecycle
   (is (thrown-with-msg? Exception #"missing configured keys"
-        (component/start (replica-block-store [:a :b])))))
+        (component/start (replica-block-store [:a :b]))))
+  (let [store (replica-block-store [:a])]
+    (is (identical? store (component/stop store)))))
 
 
-#_
 (deftest replica-behavior
   (let [replica-1 (memory-block-store)
         replica-2 (memory-block-store)
@@ -22,25 +22,24 @@
         a (block/read! "foo bar baz")
         b (block/read! "abracadabra")
         c (block/read! "123 xyz")]
-    (block/put! store a)
-    (block/put! store b)
-    (block/put! store c)
-    (is (= 3 (count (block/list replica-1))))
-    (is (every? (partial block/get replica-1)
+    @(block/put! store a)
+    @(block/put! store b)
+    @(block/put! store c)
+    (is (= 3 (count (block/list-seq replica-1))))
+    (is (every? (comp deref (partial block/get replica-1))
                 (map :id [a b c]))
         "all blocks are stored in replica-1")
-    (is (= 3 (count (block/list replica-2))))
-    (is (every? (partial block/get replica-2)
+    (is (= 3 (count (block/list-seq replica-2))))
+    (is (every? (comp deref (partial block/get replica-2))
                 (map :id [a b c]))
         "all blocks are stored in replica-2")
-    (is (= 3 (count (block/list store))))
+    (is (= 3 (count (block/list-seq store))))
     (block/delete! replica-1 (:id a))
     (block/delete! replica-2 (:id c))
-    (is (= 3 (count (block/list store)))
+    (is (= 3 (count (block/list-seq store)))
         "replica lists all available blocks")))
 
 
-#_
 (deftest ^:integration check-behavior
   (bst/check-store
     #(replica-block-store
